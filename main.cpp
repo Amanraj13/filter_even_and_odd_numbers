f9acea55a12c69355ad00d23642afe2ca9f40146
Aman Raj <amanraj05a@gmail.com>
2021-02-06 18:50:52 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,54 @@
+//program to sort even number 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int choice;
+    int upper_limit;
+    
+    //asking the user whether he wants to sort out even number or odd number
+    cout<<"\nTo sort even number press 1 and to sort odd number press 2:";
+    cin>>choice;
+    
+    if(choice==1){
+    //taking the upper limit from the user
+    cout<<"\nEnter the upper limit:";
+    cin>>upper_limit;
+    
+    
+    //for loop to filter even number;
+    cout<<"\nThe even numbers smaller than "<<upper_limit<<" are:"<<endl;
+    for(int i{1};i<upper_limit;i++){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+   }
+        
+}
+    
+    else if(choice==2){
+    //taking the upper limit from the user
+    cout<<"\nEnter the upper limit:";
+    cin>>upper_limit;
+    
+    //for loop to sort out odd number
+    cout<<"\nThe odd numbers smaller than "<<upper_limit<<" are:"<<endl;
+    for(int i{1};i<upper_limit;i++){
+        if(i%2!=0){
+            cout<<i<<endl;
+        }
+    
+    }
+    
+        
+}
+    
+    else{
+        cout<<"\nInvalid Choice";
+    }
+    
+    
+   return 0; 
+    
+}
+
